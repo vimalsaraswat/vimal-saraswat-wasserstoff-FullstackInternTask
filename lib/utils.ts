9d808e8856896d1dd9f965da1c3b9f95b031d569
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-08-25 18:24:57 +0530

    Create function to convert temperature unit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function convertTemperature(temp: number, unit: "C" | "F") {
+  if (unit === "C") {
+    return (temp - 273.15).toFixed(1);
+  } else {
+    return (((temp - 273.15) * 9) / 5 + 32).toFixed(1);
+  }
 }
